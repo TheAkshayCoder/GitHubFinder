1b2807042c5281ee047c9e0be40c6c4f0360cff7
Akshay Thakur <thakurakshay8375@gmail.com>
2021-12-16 08:53:39 +0530

    added clear button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import { Spinner } from './components/layout/Spinner';
 import axios from 'axios';
+import Search from './components/users/Search';
 
 import './App.css';
 
@@ -20,13 +21,21 @@ componentDidMount(){
     axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`).then(res => this.setState({users:res.data})).then(()=>this.setState({loading:false}))
 }
 
+searchUsers=(text)=>{
+console.log(text)
+axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`).then(res => this.setState({users:res.data.items})).then(()=>this.setState({loading:false}))
+}
+
+clearUsers=()=>{
+  this.setState({users:[]})
+}
 
   render(){
     return (
       <div className="App">
         <Navbar/>
         <div className="container">
-        
+        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length>0?true:false}/>
         {this.state.loding?<Spinner/>:this.state.users.map((user,i)=><Users loading={this.state.loading} key={i} users={this.state.users}/>)}
         </div>
       </div>
---
+++ src/components/users/Search.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export class Search extends Component {
+    state={
+        text:""
+    }
+
+    static propTypes = {
+        searchUsers:PropTypes.func.isRequired,
+        showClear:PropTypes.bool.isRequired,
+        clearUsers:PropTypes.func.isRequired
+    }
+
+    onChange = (e) => {this.setState({[e.target.name]:e.target.value})}
+    onSubmit = (e) => {e.preventDefault()
+        this.props.searchUsers(this.state.text)
+        this.setState({[e.target.name]:""})
+    }
+
+    onClear = (e) =>{
+        e.preventDefault()
+        this.props.clearUsers()
+    }
+    render() {
+        return (
+            <div>
+                <form className="form">
+                    <input type="text" name="text" value={this.state.text} onChange={this.onChange} placeholder="Search Users..."/>
+                    <input type="submit" value="Search" onClick={this.onSubmit} className="btn btn-dark btn-block"/> 
+                    {this.props.showClear&&<input type="submit" value="Clear" onClick={this.onClear} className="btn btn-grey btn-block"/> }
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search
