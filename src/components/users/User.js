cf67a363704d94e6c4cd641daf754db761aff849
Akshay Thakur <thakurakshay8375@gmail.com>
2021-12-30 22:43:32 +0530

    added back button to the single user info page and also added the hireable condition

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Fragment,Component } from 'react'
+import { Spinner } from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import Link from 'react-router-dom/Link';
 
 export class User extends Component {
+
+    componentDidMount(){
+        this.props.getUsers(this.props.match.params.login);
+    }
+
+    static propTypes={
+        loading:PropTypes.bool,
+        user:PropTypes.object.isRequired,
+        getUsers:PropTypes.func.isRequired
+    }
+
     render() {
+        const {name,avatar_url,location,bio,blog,login,html_url,followers,following,public_repos,public_gists,hireable}=this.props.user
+        const {loading}=this.props;
+        if (loading) return <Spinner/>
         return (
-            <div>
-                User
-            </div>
+            <Fragment>
+                <Link to='/' className='btn btn-light'>
+                    Back To Search
+                </Link>Available to hire : {' '}
+                {hireable?<span style={{color:"green",fontWeight:700}}>Yes</span>:<span style={{color:"red",fontWeight:700}}>No</span>}
+            </Fragment>
         )
     }
 }
